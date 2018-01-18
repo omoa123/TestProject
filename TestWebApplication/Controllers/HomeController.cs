f3a3bbe91fae63c0fb12c0808eb3714c721c130f
owen.chou <owen.chou@presco.ws>
2018-01-18 16:18:48 +0800

    add log attribute

--- TestWebApplication/Controllers/HomeController.cs
+++ TestWebApplication/Controllers/HomeController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestWebApplication.Attribute;
 
 namespace TestWebApplication.Controllers
 {
+    [Log]
     public class HomeController : Controller
     {
         public ActionResult Index()
---
+++ TestWebApplication/Lib/LogAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace TestWebApplication.Attribute
+{
+    public class LogAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            // 紀錄 action 執行完成的 Log
+            base.OnActionExecuted(filterContext);
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // 紀錄 action 執行的 Log
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
